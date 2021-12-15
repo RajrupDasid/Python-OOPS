5d8e8a02f1717151269ecf1c8d63b891a6c6a508
RajrupDasid <rajrupdasid@outlook.com>
2021-12-15 18:54:34 +0530

    some functions yelled

--- index.py
+++ index.py
@@ -3,6 +3,25 @@ class Customer:
         self.name=name
         self.membership_type=membership_type
         print("customer created")
+    
+
+    def update_membership(self,new_membership):
+        self.membership_type=new_membership
+
+    def __str__(self):
+        print("Converting to string")
+        return self.name+" "+self.membership_type
+   # def print_all_customers(customers):
+    #        for customer in customers:
+     #           print(customer)
+    def __eq__(self, other):
+        if self.name==other.name and self.membership_type==other.membership_type:
+            return True
+        return False
+
+    #__hash__=None
+    #__repr_=__str__
+
 
 #c=Customer("Lara","Gold")
 #c2=Customer("Ruuh","Allo")
@@ -12,6 +31,21 @@ class Customer:
 
 #list of customers
 customers=[Customer("Lara","Diamond"), Customer("Ruuh","Platinum")]
+#Customer.print_all_customers(customers)
+print(customers[0]==customers[1])
 
+
+data={customers[0]}
+
+
+#updating customer membership_type
+print(customers[1].membership_type)
+customers[1].update_membership("Nutron")
+
+
+print(customers[1].membership_type)
 print(customers[0].name)
 
+#Encapsulation
+#Inheritence
+#Polymorphisom
